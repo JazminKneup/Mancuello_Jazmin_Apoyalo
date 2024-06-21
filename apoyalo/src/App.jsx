7fd5c9657a6617547ede474bd06d773aa66b905c
jazminkneup <jazmin.kneup@gmail.com>
2024-06-21 18:16:38 -0400

    Apoyalo

---
+++ apoyalo/src/App.jsx
@@ -0,0 +1,44 @@
+
+import PersonCard from './components/PersonCard/PersonCard';
+
+function App() {
+  const person1 = {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 45,
+    hairColor: "Black"
+  };
+
+  const person2 = {
+    firstName: "John",
+    lastName: "Smith",
+    age: 88,
+    hairColor: "Brown"
+  };
+
+  const person3 = {
+    firstName: "Millard",
+    lastName: "Fillmore",
+    age: 50,
+    hairColor: "Brown"
+  };
+
+  const person4 = {
+    firstName: "Maria",
+    lastName: "Smith",
+    age: 62,
+    hairColor: "Brown"
+  };
+
+  return (
+    <div className="App">
+      <PersonCard {...person1} />
+      <PersonCard {...person2} />
+      <PersonCard {...person3} />
+      <PersonCard {...person4} />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ apoyalo/src/components/PersonCard/PersonCard.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/prop-types */
+import { Component } from "react";
+
+class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age
+        };
+    }
+
+    render() {
+        const { firstName, lastName, hairColor } = this.props;
+        const { age } = this.state;
+        return (
+            <div className="PersonCard">
+                <h1>{lastName}, {firstName}</h1>
+                <p>Age: {age}</p>
+                <p>Hair Color: {hairColor}</p>
+            </div>
+        );
+    }
+}
+
+export default PersonCard;
+
